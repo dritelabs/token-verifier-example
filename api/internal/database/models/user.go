141e4fb37ed3b5924c01f8d8a491fa088e387f91
Guillermo Lopez <guillermolopez2529@gmail.com>
2022-12-14 00:11:28 -0400

    feat: add create user rpc

--- api/internal/database/models/user.go
+++ api/internal/database/models/user.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	pb "github.com/dritelabs/accounts/internal/proto/v1"
 	"github.com/gofrs/uuid"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"gorm.io/gorm"
 )
 
@@ -24,24 +26,24 @@ type User struct {
 	Username                 string   `gorm:"index"`
 }
 
-// func (p *User) ToPostResponse() *pb.Post {
-// 	return &pb.Post{
-// 		Id:          p.ID.String(),
-// 		Title:       p.Title,
-// 		Description: p.Description,
-// 		CreatedAt:   timestamppb.New(p.CreatedAt),
-// 		UpdatedAt:   timestamppb.New(p.CreatedAt),
-// 	}
-// }
+func (u *User) ToUserResponse() *pb.User {
+	return &pb.User{
+		Id:        u.ID.String(),
+		Email:     u.Email,
+		Username:  u.Username,
+		CreatedAt: timestamppb.New(u.CreatedAt),
+		UpdatedAt: timestamppb.New(u.CreatedAt),
+	}
+}
 
-// func ToListPostsResponse(posts []Post) *pb.ListPostsResponse {
-// 	var res []*pb.Post
+func ToListUsersResponse(posts []User) *pb.ListUsersResponse {
+	var res []*pb.User
 
-// 	for _, p := range posts {
-// 		res = append(res, p.ToPostResponse())
-// 	}
+	for _, p := range posts {
+		res = append(res, p.ToUserResponse())
+	}
 
-// 	return &pb.ListPostsResponse{
-// 		Posts: res,
-// 	}
-// }
+	return &pb.ListUsersResponse{
+		Users: res,
+	}
+}
--- api/internal/server/create_user.go
+++ api/internal/server/create_user.go
@@ -3,23 +3,38 @@ package server
 import (
 	"context"
 
+	"github.com/dritelabs/accounts/internal/crypto"
+	"github.com/dritelabs/accounts/internal/database/models"
+	"github.com/dritelabs/accounts/internal/database/query"
 	pb "github.com/dritelabs/accounts/internal/proto/v1"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *AccountServer) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (*pb.User, error) {
-	// c := database.User{
-	// 	Email:    in.GetEmail(),
-	// 	Password: in.GetPassword(),
-	// 	Username: in.GetUsername(),
-	// }
+	log.Info().Msgf("creating user with email %s", in.GetEmail())
 
-	// err := query.Client.WithContext(ctx).Create(&c) // pass pointer of data to Create
+	hash, err := crypto.HashPassword(in.GetPassword())
 
-	// if result := s.DB.Create(&c); result.Error != nil {
-	// 	return nil, status.Errorf(codes.Internal, "failed to create user: %s", result.Error)
-	// }
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash password %s", err)
+	}
 
-	// log.Printf("Current post id %v", p.ID)
+	u := models.User{
+		Email:    in.GetEmail(),
+		Password: hash,
+		Username: in.GetUsername(),
+		Profile:  &models.Profile{},
+	}
 
-	return &pb.User{}, nil
+	err = query.User.WithContext(ctx).Create(&u) // pass pointer of data to Create
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	log.Info().Msgf("user with email %s was successfully created", in.GetEmail())
+
+	return u.ToUserResponse(), nil
 }
